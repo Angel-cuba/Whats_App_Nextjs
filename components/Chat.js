ab38b44b944a5683e52bc79240f630ebcf61e7dd
Angel <araozangel842@gmail.com>
2021-10-24 22:57:37 +0300

    List of people to chat with

---
+++ components/Chat.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import { Avatar } from '@mui/material';
+import { getRecipientEmail } from '../util/getRecipientEmail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebaseConfig';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+import { useRouter } from 'next/router';
+
+function Chat({ id, users }) {
+	const router = useRouter();
+	console.log('id--->', id);
+	console.log('users--->', users);
+	const [user] = useAuthState(auth);
+	const [recipientSnapshot] = useCollection(
+		db.collection('users').where('email', '==', getRecipientEmail(users, user))
+	);
+	console.log('user-->', user);
+	const host = recipientSnapshot?.docs?.[0]?.data();
+	const recipientEmail = getRecipientEmail(users, user);
+	console.log('host-->', host);
+	console.log('recipientEmail---->', recipientEmail);
+
+	const enterChat = () => {
+		router.push(`/chat/${id}`);
+	};
+	return (
+		<Container onClick={enterChat}>
+			{host ? (
+				<UserAvatar src={host?.photoURL || user.photoURL} />
+			) : (
+				<UserAvatar>{recipientEmail[0].toUpperCase()}</UserAvatar>
+			)}
+
+			<p>{recipientEmail}</p>
+			{/* <p>
+				{users.map((user, index) => (
+					<p key={index}>{user}</p>
+				))}
+			</p>*/}
+		</Container>
+	);
+}
+
+export default Chat;
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+	word-break: break-word;
+
+	:hover {
+		background-color: #e9eaeb;
+	}
+`;
+
+const UserAvatar = styled(Avatar)`
+	margin: 5px;
+	margin-right: 15px;
+`;
